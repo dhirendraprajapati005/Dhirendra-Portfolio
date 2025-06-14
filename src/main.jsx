8cf4517e3e3e3c3ea6dc1c5385452085f9aa7fd7
Dhirendra Prajapati <prajapatidhirendra005@gmail.com>
2025-06-15 00:47:41 +0530

    to see isight

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+   <SpeedInsights />
     <App />
   </BrowserRouter>,
 )
